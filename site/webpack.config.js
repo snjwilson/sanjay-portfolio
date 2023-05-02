28fbf42f975aacb97ab110962c90b6e7b83e7f17
snjwilson <sanjaywilsonktm@gmail.com>
2023-05-03 01:33:24 +0530

    configure basic working react app

---
+++ site/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./app/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./dist/index.html",
+    }),
+  ],
+};
