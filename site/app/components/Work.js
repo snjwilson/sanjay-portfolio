1a45129625ab1b4c3272e57a52c32d2d83e51602
snjwilson <sanjaywilsonktm@gmail.com>
2023-06-03 00:31:04 +0530

    Deploy to vercel

--- site/app/components/Work.js
+++ site/app/components/Work.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Work = () => {
   return (
     <section>
-      <h2>Some of my work will be displayed here</h2>
+      <h2>Some of my work will be displayed hereeee</h2>
     </section>
   );
 };
